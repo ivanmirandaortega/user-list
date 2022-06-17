3950102027424b3ca0e15946e1687772e073f9b1
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-16 17:21:47 -0700

    added state hook to manage user input

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 import styles from "./AddUser.module.css";
 
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  function usernameChangeHandler(event) {
+    setEnteredUsername(event.target.value);
+  }
+
   function addUserHandler(event) {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+  }
+
+  function addAgeHandler(event) {
+    setEnteredAge(event.target.value);
   }
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={addAgeHandler} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
