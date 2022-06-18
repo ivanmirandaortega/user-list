18510f730760d7c3273280771c4ddadff58f0645
ivanmirandaortega <imirandaortega.pd@gmail.com>
2022-06-17 17:10:25 -0700

    used state hook to manage the list of added users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUserList] = useState([]);
+
+  function addUserHandler(username, age, id) {
+    setUserList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: username, age: age, id: Math.random().toString() },
+      ];
+    });
+  }
   return (
     <div>
-      <AddUser />
-      <UserList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,35 +4,27 @@ import Button from "../UI/Button";
 
 import styles from "./AddUser.module.css";
 
-function AddUser() {
+function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-  //   const [isValid, setIsValid] = useState(true);
 
   function usernameChangeHandler(event) {
-    // if (event.target.value.trim().length > 0) {
-    //   setIsValid(true);
-    // }
     setEnteredUsername(event.target.value);
   }
 
   function ageChangeHandler(event) {
-    // if (event.target.value > 0) {
-    //   setIsValid(true);
-    // }
     setEnteredAge(event.target.value);
   }
 
   function addUserHandler(event) {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-      //   setIsValid(false);
       return;
     }
     if (+enteredAge < 1) {
       return;
     }
-    console.log(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
   }
